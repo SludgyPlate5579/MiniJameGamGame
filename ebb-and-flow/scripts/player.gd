7678df95f4ef8d7d1889d04b53d3cf4fd6cbf4b0
amoo2 <aliev05042007@gmail.com>
2026-02-08 17:07:27 -0500

    Movement

--- ebb-and-flow/scripts/player.gd
+++ ebb-and-flow/scripts/player.gd
@@ -3,15 +3,15 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
-
+@export var gravCoeff:float = .5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * gravCoeff * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	# Handle jump. 
+	if Input.is_action_just_pressed("ui_accept"):
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
