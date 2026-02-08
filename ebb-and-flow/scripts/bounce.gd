61f5151d69ff73023703eed554a84cffcd4a2bb5
John Xuan <rpgmage48@gmail.com>
2026-02-08 14:49:50 -0500

    made a lame bounce script

--- ebb-and-flow/scripts/bounce.gd
+++ ebb-and-flow/scripts/bounce.gd
@@ -1,8 +1,34 @@
-static func bounce (sprite: Sprite2D, delta, height: int, width: int, rotation: int):
-	sprite.scale = Vector2(width, height)
-	sprite.set_rotation_degrees(rotation)
+extends Node
+
+#exported variables
+@export var sprite: Sprite2D = null
+@export var height: float = 1
+@export var width: float = 1
+@export var rotation: float = 0
+@export var bounceSpeed: float = 5
+
+#code specific variables
+var initScale
+var initRotation
+var isBounce: bool = false
+
+func _ready() -> void:
+	initScale = Vector2(sprite.scale.x, sprite.scale.y)
+	initRotation = sprite.rotation
+
+func _process(delta: float) -> void:
+	if (sprite.scale == initScale && sprite.rotation == initRotation):
+		isBounce = true
+	elif (sprite.scale == Vector2(width, height) && sprite.rotation == rotation):
+		isBounce = false
 	
-	sprite.rotation = move_toward(sprite.rotation, 1, 1 * delta)
-	sprite.scale.x = move_toward(sprite.scale.x, 1, 1 * delta)
-	sprite.scale.y = move_toward(sprite.scale.y, 1, 1 * delta)
-	return
+	if (isBounce):
+		sprite.rotation = move_toward(sprite.rotation, rotation, bounceSpeed * delta)
+		sprite.scale.x = move_toward(sprite.scale.x, width, bounceSpeed * delta)
+		sprite.scale.y = move_toward(sprite.scale.y, height, bounceSpeed * delta)
+	else:
+		sprite.rotation = move_toward(sprite.rotation, initRotation, bounceSpeed * delta)
+		sprite.scale.x = move_toward(sprite.scale.x, initScale.x, bounceSpeed * delta)
+		sprite.scale.y = move_toward(sprite.scale.y, initScale.y, bounceSpeed * delta)
+		
+	
